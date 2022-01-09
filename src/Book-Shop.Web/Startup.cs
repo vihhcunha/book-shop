96e9483fc7ee675634486ac2bbd8f7b6b8a26b07
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2022-01-09 17:34:03 -0300

    Add startup class

---
+++ src/Book-Shop.Web/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace Book_Shop.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(routes =>
+            {
+                routes.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapControllers();
+            });
+        }
+    }
+}
