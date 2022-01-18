7b1e219ec8cac60760eed82356e6fdb2abd9c19e
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2022-01-18 07:31:48 -0300

    Improving products and providers views

---
+++ src/Book-Shop.Web/Extensions/RazorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Book_Shop.Web.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormatDoc(this RazorPage page, int personKind, string doc)
+        {
+            return personKind == 1 ? Convert.ToUInt64(doc).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(doc).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
