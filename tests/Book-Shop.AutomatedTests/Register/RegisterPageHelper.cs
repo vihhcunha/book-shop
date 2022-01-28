11fdc8dd1bc51c18b4ffc9f348b94f1591992141
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2022-01-28 08:32:33 -0300

    Add automated tests for register page

---
+++ tests/Book-Shop.AutomatedTests/Register/RegisterPageHelper.cs
@@ -0,0 +1,64 @@
+﻿using Bogus;
+using Book_Shop.AutomatedTests.Config;
+using Book_Shop.AutomatedTests.Extensions;
+
+namespace Book_Shop.AutomatedTests.Register
+{
+    public class RegisterPageHelper
+    {
+        public string EmailSpanXPath => "/html/body/div/main/form/div/div[1]/div/span/span";
+        public string PasswordSpanXPath => "/html/body/div/main/form/div/div[2]/div/span";
+        public string ConfirmPasswordSpanXPath => "/html/body/div/main/form/div/div[3]/div/span/span";
+        public string PageTitleH3 => "/html/body/div/main/form/h3";
+        public string ValidEmail { get; }
+        public string ValidPassword { get; }
+
+        private readonly SeleniumHelper _helper;
+        public RegisterPageHelper(SeleniumHelper seleniumHelper)
+        {
+            _helper = seleniumHelper;
+            ValidPassword = new Faker().Internet.PasswordCustom();
+            ValidEmail = new Faker().Internet.Email();
+        }
+
+        public void AccessRegisterPage()
+        {
+            _helper.GoToUrl(_helper.Configuration.RegisterUrl);
+        }
+
+        public void FillEmailField(string value)
+        {
+            _helper.FillTextBoxById("Input_Email", value);
+        }
+
+        public void FillPasswordField(string value)
+        {
+            _helper.FillTextBoxById("Input_Password", value);
+        }
+
+        public void FillConfirmPasswordField(string value)
+        {
+            _helper.FillTextBoxById("Input_ConfirmPassword", value);
+        }
+
+        public void SubmitRegisterForm()
+        {
+            _helper.ClickOnButtonById("registerSubmit");
+        }
+
+        public string GetEmailSpanText()
+        {
+            return _helper.GetElementByXPath(EmailSpanXPath).Text;
+        }
+
+        public string GetPasswordSpanText()
+        {
+            return _helper.GetElementByXPath(PasswordSpanXPath).Text;
+        }
+
+        public string GetConfirmPasswordSpanText()
+        {
+            return _helper.GetElementByXPath(ConfirmPasswordSpanXPath).Text;
+        }
+    }
+}
---
+++ tests/Book-Shop.AutomatedTests/Register/RegisterTests.cs
@@ -0,0 +1,95 @@
+﻿using Book_Shop.AutomatedTests.Config;
+using System;
+using Xunit;
+
+namespace Book_Shop.AutomatedTests.Register
+{
+    [Collection(nameof(AutomationWebFixtureCollection))]
+    public class RegisterTests
+    {
+        private readonly AutomationWebTestsFixture _testsFixture;
+        private readonly RegisterPageHelper _pageHelper;
+        private string _urlRegistrationPage;
+
+        public RegisterTests(AutomationWebTestsFixture fixture)
+        {
+            _testsFixture = fixture;
+            _pageHelper = new RegisterPageHelper(fixture.BrowserHelper);
+        }
+
+        [Fact(DisplayName = "User can access")]
+        [Trait("Category", "RegistrationPage")]
+        public void RegistrationPage_AccessRegistrationPage_UserCanAccess()
+        {
+            _pageHelper.AccessRegisterPage();
+
+            Assert.Contains("Create a new account", _testsFixture.BrowserHelper.GetElementByXPath(_pageHelper.PageTitleH3).Text, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact(DisplayName = "Submit empty values - show errors")]
+        [Trait("Category", "RegistrationPage")]
+        public void RegistrationPage_SubmitEmptyValues_ShowErrorMessages()
+        {
+            _pageHelper.AccessRegisterPage();
+            _pageHelper.FillEmailField("");
+            _pageHelper.FillPasswordField("");
+            _pageHelper.FillConfirmPasswordField("");
+            _pageHelper.SubmitRegisterForm();
+
+            Assert.Contains("required", _pageHelper.GetEmailSpanText(), StringComparison.InvariantCultureIgnoreCase);
+            Assert.Contains("required", _pageHelper.GetPasswordSpanText(), StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact(DisplayName = "Submit invalid password - show errors")]
+        [Trait("Category", "RegistrationPage")]
+        public void RegistrationPage_SubmitInvalidPassword_ShowErrorMessages()
+        {
+            _pageHelper.AccessRegisterPage();
+            _pageHelper.FillEmailField(_pageHelper.ValidEmail);
+            _pageHelper.FillPasswordField("aaa");
+            _pageHelper.FillConfirmPasswordField("");
+            _pageHelper.SubmitRegisterForm();
+
+            Assert.Contains("characters", _pageHelper.GetPasswordSpanText(), StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact(DisplayName = "Submit invalid email - show errors")]
+        [Trait("Category", "RegistrationPage")]
+        public void RegistrationPage_SubmitInvalidEmail_ShowErrorMessages()
+        {
+            _pageHelper.AccessRegisterPage();
+            _pageHelper.FillEmailField("aaa");
+            _pageHelper.FillPasswordField("");
+            _pageHelper.FillConfirmPasswordField("");
+            _pageHelper.SubmitRegisterForm();
+
+            Assert.Contains("valid", _pageHelper.GetEmailSpanText(), StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact(DisplayName = "Submit different passwords - show errors")]
+        [Trait("Category", "RegistrationPage")]
+        public void RegistrationPage_SubmitDifferentPasswords_ShowErrorMessages()
+        {
+            _pageHelper.AccessRegisterPage();
+            _pageHelper.FillEmailField(_pageHelper.ValidEmail);
+            _pageHelper.FillPasswordField("abcdefg");
+            _pageHelper.FillConfirmPasswordField("gfedcba");
+            _pageHelper.SubmitRegisterForm();
+
+            Assert.Contains("match", _pageHelper.GetConfirmPasswordSpanText(), StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact(DisplayName = "Submit form")]
+        [Trait("Category", "RegistrationPage")]
+        public void RegistrationPage_SubmitForm_ShouldBeOk()
+        {
+            _pageHelper.AccessRegisterPage();
+            _pageHelper.FillEmailField(_pageHelper.ValidEmail);
+            _pageHelper.FillPasswordField(_pageHelper.ValidPassword);
+            _pageHelper.FillConfirmPasswordField(_pageHelper.ValidPassword);
+            _pageHelper.SubmitRegisterForm();
+
+            Assert.Contains("Welcome", _testsFixture.BrowserHelper.GetElementByXPathWaitingForTitle("/html/body/div/main/div/h1", "Home Page").Text, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
